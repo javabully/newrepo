ddd1a1b10e924d61f5ce86008db4c20822bf60e4
PChapman <paul.chapman@inmobi.com>
2012-12-11 13:36:37 +0000

    again

--- com.sorted.test.lib/src/com/sorted/test/lib/LibStrings.java
+++ com.sorted.test.lib/src/com/sorted/test/lib/LibStrings.java
@@ -5,7 +5,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class LibStrings {
-	private static final String BUNDLE_NAME = "PluginResources11122";
+	private static final String BUNDLE_NAME = "PluginResources";
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 	
 	/**
